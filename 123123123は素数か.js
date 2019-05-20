409acaf939d9c662d7ed69ca9482e4485cb122c1
NakanishiKenta <nakaken.1999@icloud.com>
2019-05-20 20:16:37 +0900

    htmlファイルを追加しました

---
+++ 123123123は素数か.js
@@ -0,0 +1,25 @@
+var x="123123123"
+if(x%2===0){
+    console.log("2の倍数");
+}else if(x%3===0){
+    console.log("3の倍数");
+}else{
+    console.log("2，3の倍数ではない");
+}
+
+var a="123123123";
+for(var b=2; b<123123123; b++)
+for(var c=2; c<123123123; c++)
+
+if(a%b===0&&a%c===0){
+    console.log("素数ではない1");
+}else if(a%b===0){
+    console.log("素数ではない2");
+}else if(a%c===0){
+    console.log("素数ではない3");
+}else{
+    console.log("素数である");
+}
+
+
+
---
+++ 123123123は素数か2.js
@@ -0,0 +1,7 @@
+var x="123123123";
+for(var a=2; a<=123123123/2; a++){
+if(x%a>0&&x%a<123123123/2){
+    continue;
+}console.log(a);
+}
+
